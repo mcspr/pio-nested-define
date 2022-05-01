47ac2aafcc059bac6188131608cf91f06fac3b02
Maxim Prokhorov <prokhorov.max@outlook.com>
2022-05-01 15:05:56 +0300

    fixup! init

---
+++ src/extra.cpp
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+#include "extra.h"
+
+#if SELECTED_VARIANT == VARIANT_ONE
+#include <testlib.h>
+
+void test() {
+}
+#endif
---
+++ src/extra.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VARIANT_ONE 1
+#define VARIANT_TWO 2
+
+void test();
